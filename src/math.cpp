7ffec88d9ed87e947c1bbc49edcb96a4a620c1da
chimi <deadxraver@yandex.ru>
2026-03-23 22:41:43 +0300

    other functions with help of sin+ln

--- src/math.cpp
+++ src/math.cpp
@@ -21,7 +21,7 @@ double base_math::sin(double x, int eps) const {
 
 double base_math::ln(double x, int eps) const {
   if (x <= 0)
-    throw std::string("recieved x <= 0 in ln");
+    throw std::string("ln: recieved x <= 0 in ln");
   int k = 0;
   for (; x >= 2; x /= 2, ++k);
   for (; x < 1; x *= 2, --k);
@@ -33,4 +33,19 @@ double base_math::ln(double x, int eps) const {
     res += term / i;
   }
   return res + k * M_LN2;
+}
+
+double ext_math::cos(double x, int eps) const {
+  return std::pow(1 - std::pow(sin(x, eps), 2), 0.5);
+}
+
+double ext_math::sec(double x, int eps) const {
+  double cos_val = cos(x, eps);
+  if (cos_val == 0)
+    throw std::string("sec: cos(x) should not be 0");
+  return 1 / cos_val;
+}
+
+double ext_math::log(double x, double base, int eps) const {
+  return ln(x, eps) / ln(base, eps);
 }
--- src/math.hpp
+++ src/math.hpp
@@ -5,3 +5,10 @@ public:
   virtual double sin(double x, int eps) const;
   virtual double ln(double x, int eps) const;
 };
+
+class ext_math : public base_math {
+public:
+  virtual double cos(double x, int eps) const;
+  virtual double sec(double x, int eps) const;
+  virtual double log(double x, double base, int eps) const;
+};

37c9e9097259ed6398a8dbf80f446bd79329871a
chimi <deadxraver@yandex.ru>
2026-04-06 20:41:20 +0300

    check that sec do not use lns and logs do not use cos

--- test/base_test.cpp
+++ test/base_test.cpp
@@ -18,6 +18,7 @@ TEST(math_base_test, log_uses_lns) {
   mock_base_math mbmath(PRECISION);
   EXPECT_CALL(mbmath, ln(LN_ARG)).WillOnce(::testing::Return(LN_ARG_RES));
   EXPECT_CALL(mbmath, ln(LN_BASE)).WillOnce(::testing::Return(LN_BASE_RES));
+  EXPECT_CALL(mbmath, cos(LN_BASE)).Times(0);
   double res = mbmath.log(LN_ARG, LN_BASE);
   EXPECT_NEAR(res, LN_ARG_RES / LN_BASE_RES, 1e-6);
 }
@@ -25,6 +26,7 @@ TEST(math_base_test, log_uses_lns) {
 TEST(math_base_test, sec_uses_cos) {
   mock_base_math mbmath(PRECISION);
   EXPECT_CALL(mbmath, cos(COS_ARG)).WillOnce(::testing::Return(COS_RES));
+  EXPECT_CALL(mbmath, ln(COS_ARG)).Times(0);
   double res = mbmath.sec(COS_ARG);
   EXPECT_NEAR(res, 1 / COS_RES, 1e-6);
 }
